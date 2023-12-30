eba5cbca257ca7506dbe445ef089a8119db965b4
gtxzsxxk <2524395907@qq.com>
2023-12-31 00:52:07 +0800

    test: add validation for the keyboard sender

---
+++ key_test.py
@@ -0,0 +1,42 @@
+import pyautogui
+import time
+
+print("移动鼠标到游戏，HOLD3秒")
+input("准备好回车")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep(1)
+currentMouseX, currentMouseY = pyautogui.position()
+print("鼠标坐标位置：", currentMouseX, currentMouseY)
+
+print("通过点击获取焦点")
+pyautogui.click()
+
+
+print("加速1档")
+pyautogui.keyDown('A')
+time.sleep(0.1)
+pyautogui.keyUp('A')
+print("加速2档")
+pyautogui.keyDown('A')
+time.sleep(0.1)
+pyautogui.keyUp('A')
+
+print("减速1档")
+pyautogui.keyDown('D')
+time.sleep(0.1)
+pyautogui.keyUp('D')
+
+print("加速2档")
+pyautogui.keyDown('A')
+time.sleep(0.1)
+pyautogui.keyUp('A')
+
+print("加速3档")
+pyautogui.keyDown('A')
+time.sleep(0.1)
+pyautogui.keyUp('A')
